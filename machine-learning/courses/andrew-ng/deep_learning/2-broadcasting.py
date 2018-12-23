54410bb058236e7100e2c9326919c44fbdcbbb5c
send2manoo <send2manoo@gmail.com>
2018-12-23 14:23:51 +0530

    add andrew-ng in dec 23 2018

---
+++ machine-learning/courses/andrew-ng/deep_learning/2-broadcasting.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+
+
+
+
+foods = np.array([[56,0,4.4,68.0],
+          [1.2,104,52,8.0],
+          [1.8,135,99,0.9]])
+
+
+cal = np.sum(foods,axis = 0)
+
+
+percentage = foods/cal.reshape(1,4)
+
+percentage = percentage*100
+
+
+
+a = np.random.randn(2, 3) # a.shape = (2, 3)
+b = np.random.randn(2, 1) # b.shape = (2, 1)
+c = a + b
+
+a = np.random.randn(4, 3) # a.shape = (4, 3)
+b = np.random.randn(3, 2) # b.shape = (3, 2)
+c = a*b
+
+
+a = np.random.randn(12288, 150) # a.shape = (12288, 150)
+b = np.random.randn(150, 45) # b.shape = (150, 45)
+c = np.dot(a,b)
+
+
+a = np.random.randn(3, 3)
+b = np.random.randn(3, 1)
+c = a*b
+
+
+
+# This is a 3 by 3 by 2 array, typically images will be (num_px_x, num_px_y,3) where 3 represents the RGB values
+image = np.array([[[ 0.67826139,  0.29380381],
+        [ 0.90714982,  0.52835647],
+        [ 0.4215251 ,  0.45017551]],
+
+       [[ 0.92814219,  0.96677647],
+        [ 0.85304703,  0.52351845],
+        [ 0.19981397,  0.27417313]],
+
+       [[ 0.60659855,  0.00533165],
+        [ 0.10820313,  0.49978937],
+        [ 0.34144279,  0.94630077]]])
+
+print ("image2vector(image) = " + str(image2vector(image)))
+
